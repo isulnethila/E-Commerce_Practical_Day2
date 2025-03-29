853e198039a886c18f98a15f56243db03f230799
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-29 12:31:47 +0530

    create constructors

--- myapp/src/main/java/lk/ac/vau/fas/ict/model/Student.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/model/Student.java
@@ -1,5 +1,18 @@
 package lk.ac.vau.fas.ict.model;
 
 public class Student {
+    private String regNo;
+    private String name;
+    private int age;
+    private String course;
+    private double gpa;
+    public Student(String regNo, String name, int age, String course, double gpa) {
+        this.regNo = regNo;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.gpa = gpa;
+    }
+    
 
 }
