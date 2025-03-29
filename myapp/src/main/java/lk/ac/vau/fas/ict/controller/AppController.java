c2d5ff0ccb2d88f5aac747e9410eb24be7537ce9
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-29 12:49:25 +0530

    define MyAge function

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -1,6 +1,7 @@
 package lk.ac.vau.fas.ict.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,11 @@ public class AppController {
 	@GetMapping("/name")
 	public String MyName() {
 		return "My name is SpringBoot";
+	}
+
+	@GetMapping("/age/{ag}")
+	public String MyAge(@PathVariable("ag") int age){
+		return "My age is " + age;
 	}
 
 }
