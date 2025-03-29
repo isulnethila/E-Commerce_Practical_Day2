2cf542ebac94a4f9864e64b53e5ac88a9973bd18
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-29 15:00:33 +0530

    add a new user function

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -2,6 +2,8 @@ package lk.ac.vau.fas.ict.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,4 +79,18 @@ public class AppController {
 		}
 		return filterStudents;
 	}
+
+	@GetMapping("/students/sort/gpa")
+	public List<Student> getStudentsSortedByGpa(){
+		students.sort((s1,s2) -> Double.compare(s2.getGpa(), s1.getGpa()));
+		return students;
+	}
+
+	@PostMapping("/students/add")
+	public List<Student> addStudent(@RequestBody Student student){
+		students.add(student);
+		return students;
+	}
+
+
 }
