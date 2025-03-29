7e527305826796675ae6f70966437ee6e0629764
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-29 15:35:12 +0530

    add update and delete functions

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -1,8 +1,10 @@
 package lk.ac.vau.fas.ict.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -92,5 +94,29 @@ public class AppController {
 		return students;
 	}
 
+	@PutMapping("/students/update/{regNo}")
+	public Student updateStudent(@PathVariable("regNo") String regNo,@RequestBody Student updateStudent){
+		for(Student student:students){
+			if(student.getRegNo().equals(regNo)){
+				student.setName(updateStudent.getName());
+				student.setAge(updateStudent.getAge());
+				student.setCourse(updateStudent.getCourse());
+				student.setGpa(updateStudent.getGpa());
+				return student;
+			}
+		}
+		return null;
+	}
+
+	@DeleteMapping("/students/delete/{regNo}")
+	public String delelteStudent(@PathVariable("regNo") String regNo){
+		for(Student student:students){
+			if(student.getRegNo().equals(regNo)){
+				students.remove(student);
+				return "Student with regNo " + regNo + " deleted successfully";
+			}
+		}
+		return "Student with regNo " + regNo + " not found";
+	}
 
 }
